c5f582c0c0bad5c6f201bc4d6188c12511ff1bc8
Kirilov Vladislav <kirilov.vl.ol@gmail.com>
2020-11-23 10:59:08 +0300

    Updates

---
+++ precache-manifest.4ce94f3e645e332a5f0d2404beded034.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd6583109ede90c96e0af89f27db40f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "451103d94388212f7bb5",
+    "url": "/resume/static/css/main.e779ab6b.chunk.css"
+  },
+  {
+    "revision": "b7e8693369d0a32cb4c7",
+    "url": "/resume/static/js/2.d83e719c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/resume/static/js/2.d83e719c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451103d94388212f7bb5",
+    "url": "/resume/static/js/main.9e2f9586.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "4efd57d95b50bc74a040b9233d85a357",
+    "url": "/resume/static/media/avatar.4efd57d9.jpeg"
+  }
+]);
